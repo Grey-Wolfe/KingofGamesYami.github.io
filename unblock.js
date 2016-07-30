b3ddbe2af946cb8f279ac89e5a892f8626f2eb25
KingofGamesYami <KingofGamesYami@users.noreply.github.com>
2016-07-29 21:09:19 -0500

    Create unblock.js

---
+++ unblock.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+  var source = prompt("Please enter your URL", "http://www.duelingnetwork.com/");
+  demo.src = source
+</script>
+<iframe src="", width="100%", height="100%", id="demo"></iframe>
+
+</body>
+</html>
